2f53b80ec848e8bfa5982a88a7cda50baf546c3e
Caelan <caelanrp@gmail.com>
2019-10-18 23:13:43 -0700

    i hate cameras

--- Assets/Scripts/HouseGen.cs
+++ Assets/Scripts/HouseGen.cs
@@ -6,16 +6,32 @@ public class HouseGen : MonoBehaviour
 {
     public Vector2 origin;
     public int gridSizeX, gridSizeY;
+    public float tileSize = 0.1f;
+
+    Tile[,] tiles;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Awake(){
+        tiles = new Tile[gridSizeX, gridSizeY];
+        InitGrid();
+    }
+
+    void InitGrid(){
+        for(int x = 0; x < gridSizeX; x++){
+            for(int y = 0; y < gridSizeY; y++){
+                tiles[x,y] = Tile.CreateTile(0);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public Vector2 GetWorldPosition(int x, int y){
+        return Vector2.zero;
+        // TODO 
     }
 
     void GenerateFullBorder(){
@@ -26,9 +42,5 @@ public class HouseGen : MonoBehaviour
                 }
             }
         }
-    }
-
-    void GenerateTile(int gridX, int gridY, int tilesetX, int tilesetY){
-        
     }
 }
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private static Tile _T;
+    public static Tile prefab{
+        get{
+            if (_T == null){
+                _T = Resources.Load<GameObject>("Tile").GetComponent<Tile>();
+            }
+            return _T;
+        }
+    }
+
+    private static Sprite[] _S;
+    public static Sprite[] sprites{
+        get{
+            if (_S == null){
+                _S = Resources.LoadAll<Sprite>("Tileset_Default");
+            }
+            return _S;
+        }
+    }
+
+    SpriteRenderer spr;
+
+    void Awake(){
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    public static Tile CreateTile(int tileIndex){
+        Tile newTile = Instantiate(prefab);
+        newTile.SetSprite(tileIndex);
+        return newTile;
+    }
+
+    public void SetSprite(int spriteIndex){
+        if (spriteIndex == -1){
+            spr.sprite = null;
+            return;
+        }
+        try{
+            spr.sprite = sprites[spriteIndex];
+        } catch (Exception e){
+            Debug.LogError(e);
+        }
+    }
+}
