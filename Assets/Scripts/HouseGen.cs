e2a34d5eb4ecd7d570147d4bba2a075c69c8970e
Caelan <caelanrp@gmail.com>
2019-10-18 22:32:24 -0700

    Initial Commit

---
+++ Assets/Scripts/HouseGen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseGen : MonoBehaviour
+{
+    public Vector2 origin;
+    public int gridSizeX, gridSizeY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateFullBorder(){
+        for(int x = 0; x < gridSizeX; x++){
+            for(int y = 0; y < gridSizeY; y++){
+                if (x == 0 || y == 0 || x == gridSizeX-1 || y == gridSizeY-1){
+
+                }
+            }
+        }
+    }
+
+    void GenerateTile(int gridX, int gridY, int tilesetX, int tilesetY){
+        
+    }
+}
---
+++ Assets/Scripts/TextScroller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using NaughtyAttributes;
+public class TextScroller : MonoBehaviour
+{
+    public int charsPerSecond;
+
+    TextMeshPro textMesh;
+
+    void Awake(){
+        textMesh = GetComponentInChildren<TextMeshPro>();
+    }
+    IEnumerator BloopText(string text){
+        TMPro.TMP_TextInfo textInfo = textMesh.textInfo;
+        int totalVisibleCharacters = textInfo.characterCount; // Get # of Visible Character in text object
+        float currentIndex = 0;
+        int intIndex = 0;
+        textMesh.text = text;
+        textMesh.maxVisibleCharacters = 0;
+        yield return null;
+        while (currentIndex < totalVisibleCharacters)
+        {
+            currentIndex = Mathf.Min (currentIndex + (charsPerSecond * Time.deltaTime), totalVisibleCharacters);
+            var i = Mathf.FloorToInt(currentIndex);
+            if (i > intIndex){
+                // TODO audio bloops
+                intIndex = i;
+            }
+            textMesh.maxVisibleCharacters = intIndex;
+            yield return null;
+        }
+    }
+
+    public string testString;
+
+    [Button("Test Text")]
+    void TestText(){
+        StartCoroutine(BloopText(testString));
+    }
+
+
+}
