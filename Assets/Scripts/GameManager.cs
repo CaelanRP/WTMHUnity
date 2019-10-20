0c88226282659d68a3f2d4120645396475fc546d
Caelan <caelanrp@gmail.com>
2019-10-20 15:39:01 -0700

    added all kinds of goodies

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float textDelay;
+    void Start()
+    {
+        StartCoroutine(EnterHouseRoutine());
+    }
+
+    IEnumerator EnterHouseRoutine(){
+        HouseGen.instance.GenerateHouse();
+        yield return new WaitForSeconds(textDelay);
+        TextScroller.instance.BloopText("WELCOME TO MY HOUSE");
+    }
+}
