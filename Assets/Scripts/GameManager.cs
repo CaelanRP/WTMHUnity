f5eb26ed4d40436247caf08f87d8094a3bac169b
Caelan <caelanrp@gmail.com>
2019-10-20 17:14:10 -0700

    trying out some player animation stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public float textDelay;
+    AudioSource audioSource;
+
+    public float musicFadeTime;
+    public List<AudioClip> houseMusic;
+
+    void Awake(){
+        audioSource = GetComponent<AudioSource>();
+    }
+
     void Start()
     {
         StartCoroutine(EnterHouseRoutine());
     }
 
+    void Update(){
+
+    }
+
+    void Reset(){
+        StopAllCoroutines();
+        TextScroller.instance.StopAllCoroutines();
+        HouseGen.instance.StopAllCoroutines();
+    }
+
     IEnumerator EnterHouseRoutine(){
         HouseGen.instance.GenerateHouse();
+        PlayMusic();
+        //Player.instance.WalkIntoHouse();
         yield return new WaitForSeconds(textDelay);
         TextScroller.instance.BloopText("WELCOME TO MY HOUSE");
+    }
+
+    AudioClip prevMusic = null;
+    void PlayMusic(){
+        audioSource.Stop();
+        audioSource.clip = houseMusic.Where(c => c != prevMusic).RandomSelection();
+        prevMusic = audioSource.clip;
+        StartCoroutine(FadeInMusic(musicFadeTime));
+    }
+
+    IEnumerator FadeInMusic(float FadeTime) {
+        audioSource.Play();
+        audioSource.volume = 0f;
+        while (audioSource.volume < 1) {
+            audioSource.volume += Time.deltaTime / FadeTime;
+            yield return null;
+        }
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Animator animator;
+    public static Player instance;
+    public Vector3 outsideHousePos, insideHousePos;
+    void Awake(){
+        instance = this;
+        animator = GetComponent<Animator>();
+    }
+    Vector3 target;
+    public IEnumerator WalkToLocation(Vector3 target){
+        this.target = target;
+        animator.SetBool("Walking", true);
+        while(Vector3.Distance(transform.position, target) > 0.2f){
+            yield return null;
+        }
+        transform.position = target;
+        animator.SetBool("Walking", true);
+    }
+    public void StepForward(){
+        Vector3 newPos = Vector3.MoveTowards(transform.position, target, 0.2f);
+        //newPos = new Vector3()
+    }
+
+    public void WalkIntoHouse(){
+        transform.position = outsideHousePos;
+        StartCoroutine(WalkToLocation(insideHousePos));
+    }
+}
